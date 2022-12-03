a4dd016fc92fa44d1150e693fae5d35fea32732f
Patrick hachicho <Hachichopatrick@gmail.com>
2022-12-03 13:30:17 -0500

    added navabr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Routes } from 'react-router-dom'
-import NavBar from './components/NavBar';
+import { Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <p>Hi</p>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import './App.css'
 
 function NavBar() {
   return (
-    <div>navbar</div>
-  )
+    <Navbar bg="info" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Somewhere</Nav.Link>
+            <NavDropdown title="About Us" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Cassandra</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Franklin
+              </NavDropdown.Item>
+              {/* <NavDropdown.Divider /> */}
+              <NavDropdown.Item href="#action5">
+                Patrick
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          {/* <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
