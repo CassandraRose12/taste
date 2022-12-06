d94c0a9934e6239b1bdeddeb1e27af6c23bc1854
Patrick hachicho <Hachichopatrick@gmail.com>
2022-12-05 21:34:27 -0500

    adding css to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import React from "react";
-import jpg1 from "./photo/jpg1.jpg"
+import jpg1 from "./photo/jpg1.jpg";
+import jpg2 from "./photo/jpg2.jpg";
+import "./Home.css"
 
 function Home() {
   return (
     <div>
+      <img className="Parra" src={jpg1} alt="hi"></img>
+      <div className="home">
       <h1>Traveling Through Our TasteBuds</h1>
-      <img src={jpg1} alt="food on table"></img>
       <h2>Elevate Your Tastebuds</h2>
+      </div>
+
+      {/* <div style={{ backgroundImage: `url(${jpg2})` }}>  
+      </div> */}
     </div>
   );
 }
-
+{/* <div className="home" style={{ backgroundImage: `url(${jpg1})` }}> */}
+// add button that leads to countries
 export default Home;
