9d09e33b08e516a9072f1654ef410f51e005a8d2
Patrick hachicho <Hachichopatrick@gmail.com>
2022-12-07 20:54:29 -0500

    working on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import jpg1 from "./photo/jpg1.jpg";
-import jpg2 from "./photo/jpg2.jpg";
+// import jpg2 from "./photo/jpg2.jpg";
 import "./Home.css"
+import {Link} from 'react-router-dom'
 
 function Home() {
+
   return (
-    <div>
-      <img className="Parra" src={jpg1} alt="hi"></img>
+    <div className="head-text">
+      <div className="head-image">
+      <img className="Parra" src={jpg1} alt="food" />
+      </div>
+      <div className="text-on-image">
+        <h1>Traveling Throughout <pre></pre>The World</h1>
+         <p className="explore">Elevate Your TasteBuds <Link to="/Countries"><b>Here</b></Link></p>
+      </div>
       <div className="home">
       <h1>Traveling Through Our TasteBuds</h1>
       <h2>Elevate Your Tastebuds</h2>
@@ -17,6 +25,6 @@ function Home() {
     </div>
   );
 }
-{/* <div className="home" style={{ backgroundImage: `url(${jpg1})` }}> */}
+
 // add button that leads to countries
 export default Home;
