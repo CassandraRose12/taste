0861f712e1eb995ca9dcf799fc3aa86289dbd7dc
Patrick Hachicho <hachichopatrick@gmail.com>
2022-12-09 01:03:33 -0500

    making more changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,20 @@ import jpg2 from "./photo/jpg2.jpg";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import VideoSection from "./photo/BackVideo";
-import { FirstImage, ImageBg, ImgContent, ImgH1, ImgP1 } from "./photo/BackVideo/Elements";
+import {
+  FirstImage,
+  ImageBg,
+  ImgContent,
+  ImgH1,
+  ImgP1,
+} from "./photo/BackVideo/Elements";
+import {
+  SecondImage,
+  Image2Bg,
+  Img2Content,
+  Img2H1,
+  Img2P1,
+} from "./photo/BackVideo/Elements";
 import { ImgBg } from "./photo/BackVideo/Elements";
 
 function Home() {
@@ -13,10 +26,10 @@ function Home() {
       <VideoSection />
       <div className="home">
         <h1>Our Mission:</h1>
-          <p>
-            Our goal is to provide everyone with high quality recepies to insure
-            enjoyment.
-          </p>
+        <p>
+          Our goal is to provide everyone with high quality recepies to insure
+          enjoyment.
+        </p>
       </div>
       {/* <div>
         <img className="Parra" src={jpg1} alt="food" />
@@ -34,25 +47,29 @@ function Home() {
         <img className="jp2" src={jpg2} alt="h" />
       </div> */}
       <div>
-
-    <FirstImage>
-      <ImgBg>
-      <ImageBg src={jpg1} type="image.jpg"/>
-      </ImgBg>
-      <ImgContent>
-      <ImgH1>Travel Around The World <br></br> For Food</ImgH1>
-      <ImgP1>Elevate Your TasteBuds <Link to="/Countries">
-              <b>Here</b>
-            </Link></ImgP1>
-      </ImgContent>
-    </FirstImage>
-    </div>
+        <FirstImage>
+          <ImgBg>
+            <ImageBg src={jpg1} type="image.jpg" />
+          </ImgBg>
+          <ImgContent>
+            <ImgH1>
+              Travel Around The World <br></br> For Food
+            </ImgH1>
+            <ImgP1>
+              Elevate Your TasteBuds{" "}
+              <Link to="/Countries">
+                <b>Here</b>
+              </Link>
+            </ImgP1>
+          </ImgContent>
+        </FirstImage>
+        <SecondImage>
+          <Image2Bg src={jpg2} type="image.jpg" />
+          <Img2Content>
+          </Img2Content>
+        </SecondImage>
       </div>
-
-
-
-
-
+    </div>
 
     // <div className="head-text">
     //   <div className="head-image">
