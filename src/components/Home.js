716630125aab9503870061ef58eaa386466c1c20
Patrick Hachicho <hachichopatrick@gmail.com>
2022-12-08 19:07:01 -0500

    added video to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,61 @@
 import React from "react";
 import jpg1 from "./photo/jpg1.jpg";
-// import jpg2 from "./photo/jpg2.jpg";
-import "./Home.css"
-import {Link} from 'react-router-dom'
+import jpg2 from "./photo/jpg2.jpg";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import VideoSection from "./photo/BackVideo";
 
 function Home() {
-
   return (
-    <div className="head-text">
-      <div className="head-image">
-      <img className="Parra" src={jpg1} alt="food" />
-      </div>
-      <div className="text-on-image">
-        <h1>Traveling Throughout <pre></pre>The World</h1>
-         <p className="explore">Elevate Your TasteBuds <Link to="/Countries"><b>Here</b></Link></p>
-      </div>
+    <div>
+      <VideoSection />
       <div className="home">
-      <h1>Traveling Through Our TasteBuds</h1>
-      <h2>Elevate Your Tastebuds</h2>
+        <h1>Our Mission:</h1>
+        <div>
+          <p>
+            Our goal is to provide everyone with high quality recepies to insure
+            enjoyment.
+          </p>
+        </div>
       </div>
-
-      {/* <div style={{ backgroundImage: `url(${jpg2})` }}>  
-      </div> */}
+      <div>
+        <img className="Parra" src={jpg1} alt="food" />
+        <br />
+        <div className="text-on-image">
+          <h1>
+            Traveling Throughout <pre></pre>The World
+          </h1>
+          <p className="explore">
+            Elevate Your TasteBuds{" "}
+            <Link to="/Countries">
+              <b>Here</b>
+            </Link>
+          </p>
+        </div>
+        <img className="jp2" src={jpg2} alt="h" />
+      </div>
     </div>
+    // <div className="head-text">
+    //   <div className="head-image">
+    //     <div className="images">
+    //   <img className="Parra" src={jpg1} alt="food" />
+    //   <br></br>
+    //   <img className="jp2" src={jpg2} alt="h" />
+    //     </div>
+    //   <div className="text-on-image">
+    //     <h1>Traveling Throughout <pre></pre>The World</h1>
+    //      <p className="explore">Elevate Your TasteBuds <Link to="/Countries"><b>Here</b></Link></p>
+    //   </div>
+    //   </div>
+    //   <div className="home">
+    //   <h1>Traveling Through Our TasteBuds</h1>
+    //   <h2>Elevate Your Tastebuds</h2>
+    //   </div>
+
+    //   {/* <div style={{ backgroundImage: `url(${jpg2})` }}>
+    //   </div> */}
+    // </div>
   );
 }
 
-// add button that leads to countries
 export default Home;
---
+++ src/components/photo/BackVideo/Elements.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const HeroContainer = styled.div`
+background: #0c0c0c;
+display: flex;
+justify-content: center;
+padding: 0 30px;
+height: 800px;
+position: relative;
+z-index: 1;`
+
+export const HeroBg = styled.div`
+position: absolute;
+top: 0;
+right: 0;
+bottom 0;
+left: 0;
+width: 100%;
+height: 100%;
+overflow: hidden;
+`;
+
+export const VideoBg = styled.video`
+width: 100%;
+height: 100%;
+-o-object-fit: cover;
+object-fit: cover;
+background: #232a34;`;
+
+export const HeroContent = styled.div`
+z-index: 3;
+max-width: 1200px;
+position: absolute;
+padding: 8px 24px;
+display: flex;
+flex-direction: column;
+align-items: center;`
+
+export const HeroH1 = styled.h1`
+color: tan;
+font-size: 58px;
+text-align: center;
+margin-top: 200px;
+text-shadow: 0px 0px 2px black;
+
+@media screen and (max-width: 768px) {
+font-size 30px;
+}`
+ export const HeroP = styled.p`
+ margin-top: 24px;
+ color: #fff;
+ font-size: 34px;
+ text-align: center;
+ max-width: 600px;
+ text-shadow: 0px 0px 2px black;`
+
