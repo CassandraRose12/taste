2d9d2adedb1c944b89e8fe38eef6b6221cfe9d64
Patrick Hachicho <hachichopatrick@gmail.com>
2022-12-10 01:45:49 -0500

    about page almost done

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import ImageSlider from './ImageSlider'
-import "./About.css" 
+import React from "react";
+import ImageSlider from "./ImageSlider";
+import "./About.css";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GithubIcon from "@mui/icons-material/GitHub";
 const slides = [
-
   { url: "http://localhost:3000/Cassandra.jpg", title: "Cassandra" },
-  { url: "http://localhost:3000/jpg1.jpg", title: "Patrick" },
+  { url: "http://localhost:3000/Patrick.jpg", title: "Patrick" },
   { url: "http://localhost:3000/jpg1.jpg", title: "Franklin" },
 ];
 const containerStyles = {
@@ -14,21 +15,56 @@ const containerStyles = {
 };
 function About() {
   return (
-    <div className='idk'>
-    <div style={containerStyles} className="ImageSlider">
+    <div className="idk">
+      <h1 className="title">Meet The People Behind TATW</h1>
+      <div style={containerStyles} className="ImageSlider">
         <ImageSlider slides={slides} />
       </div>
-        <div className='Cassandra'>
-        <h1>Cassandra</h1>
+      <div>
+        <div class="row">
+          <div class="column">
+            <h2>Cassandra</h2>
+            <p>Some text..</p>
+            <a href="http://google.com"  rel="noreferrer" target="_blank">
+              {" "}
+              <LinkedInIcon />{" "}
+            </a>
+            <a href="http://google.com"  rel="noreferrer" target="_blank">
+              {" "}
+              <GithubIcon />{" "}
+            </a>
+          </div>
+          <div class="column">
+            <h2>Patrick</h2>
+            <p>Some text..</p>
+            <a
+              href="https://www.linkedin.com/in/patrick-hachicho-9b0396236/"
+              rel="noreferrer" target="_blank"
+            >
+              {" "}
+              <LinkedInIcon />{" "}
+            </a>
+            <a href="https://github.com/pchachicho"  rel="noreferrer" target="_blank">
+              {" "}
+              <GithubIcon />{" "}
+            </a>
+          </div>
+          <div class="column">
+            <h2>Franklin</h2>
+            <p>Some text..</p>
+            <a href="http://google.com"  rel="noreferrer" target="_blank">
+              {" "}
+              <LinkedInIcon />{" "}
+            </a>
+            <a href="http://google.com"  rel="noreferrer" target="_blank">
+              {" "}
+              <GithubIcon />{" "}
+            </a>
+          </div>
         </div>
-        <div className='Patrick'>
-        <h1>Patrick</h1>
-        </div>
-        <div className='Franklin'>
-        <h1>Franklin</h1>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
